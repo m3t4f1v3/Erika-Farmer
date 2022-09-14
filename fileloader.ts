235543069feeaf2fc82059a1b187feb3cc35bcf7
metafive <40429600+m3t4f1v3@users.noreply.github.com>
2022-09-14 13:14:40 +0000

    add inspire

--- fileloader.ts
+++ fileloader.ts
@@ -5,3 +5,4 @@ import "file:///home/runner/Erika-Farmer/src/commands/horny/ecchi.ts#0";
 import "file:///home/runner/Erika-Farmer/src/events/interactionCreate.ts#1";
 import "file:///home/runner/Erika-Farmer/src/events/ready.ts#1";
 import "file:///home/runner/Erika-Farmer/src/commands/kot.ts#2";
+import "file:///home/runner/Erika-Farmer/src/commands/inspire.ts#2";
--- src/commands/inspire.ts
+++ src/commands/inspire.ts
@@ -9,15 +9,16 @@ import {
 import { createCommand } from "./mod.ts";
 
 createCommand({
-  name: "kot",
-  description:
-    "Erika will send you a picture of an irl neko (cat), thinking of her master Bernkastel",
+  name: "inspire",
+  description: "Erika will send an inspirational quote",
   type: ApplicationCommandTypes.ChatInput,
   execute: async (Bot, interaction) => {
     var extension;
     if (iconBigintToHash(interaction.user.avatar!).startsWith("a_")) {
       extension = ".gif";
     }
+    const json = await (await fetch("https://zenquotes.io/api/random")).json();
+
     await Bot.helpers.sendInteractionResponse(
       interaction.id,
       interaction.token,
@@ -27,18 +28,14 @@ createCommand({
           embeds: [{
             author: {
               name: interaction.user.username,
-              url:
-                "https://static.wikia.nocookie.net/topstrongest/images/7/77/Bern_Cat.png/revision/latest/scale-to-width-down/350?cb=20200820165917",
+              url: "https://pbs.twimg.com/media/E7JrbbPXEAIfBm0.jpg",
               iconUrl:
                 `${baseEndpoints.CDN_URL}/avatars/${interaction.user.id}/${
                   iconBigintToHash(interaction.user.avatar!)
                 }${extension ?? ""}`,
             },
-            title: "I, Erika Furudo, know you're uu-uuing!",
-            description: "Meow at your screen!",
-            image: {
-              url: `https://cataas.com/cat?${Date.now()}`,
-            },
+            title: json[0]["q"],
+            description: json[0]["a"],
           }],
         },
       },
